7be1fd01898d29c95eaf03a0516a0d4af94eaac6
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2022-08-15 16:27:16 -0700

    Add JavaScript validation

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,20 @@
-// /alert("Hello");
+(function () {
+  'use strict'
+  const forms = document.querySelectorAll('.requires-validation')
+  Array.from(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          
+          //prevent form submission
+          event.preventDefault()
+          
+          event.stopPropagation()
+        }
+        form.classList.add('was-validated')
+        
+      }, false)
+    })
+  })()
+  
+  
