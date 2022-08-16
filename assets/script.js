9170179f342cc737b22042891c647e3659773ad7
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2022-08-15 17:49:44 -0700

    Load background image based on screen size

--- assets/script.js
+++ assets/script.js
@@ -15,6 +15,21 @@
         
       }, false)
     })
-  })()
-  
-  
+  })();
+
+/**
+ *  Load a different background image depending on size of screen
+ */
+
+(function () {
+  const div = document.querySelector('.bg-image'),
+    
+  // Exctracts URLs from data-bg to an array
+    src = div.getAttribute('data-bg').split(','), 
+    
+    html = document.documentElement;
+    //--bg--desktop declared in CSS, URL is declared in HTML
+    html.style.setProperty('--bg-desktop', `url(${src[0]})`);
+    //--bg--mobile declared in CSS, URL is declared in HTML
+    html.style.setProperty('--bg-mobile', `url(${src[1]})`);  
+}());
